bf26affbef9007d2d25798d5d30aea8524577449
ole380 <birdie_niels@hotmail.com>
2015-10-01 09:48:55 +0200

    Set aangemaakt, design iets aangepast

--- Assignment 2/Assignment 2/assignment2/Main.java
+++ Assignment 2/Assignment 2/assignment2/Main.java
@@ -1,64 +1,62 @@
 package assignment2;
 
 import java.util.Scanner;
+import java.io.PrintStream;
 
 public class Main {
 
 	static final String ERROR_TYPE_STRING = "error";
 	static final String ASSIGNMENT_TYPE_STRING = "assignment";
 	static final String PRINT_STATEMENT_TYPE_STRING = "print_statement";
+	static final String COMMENT_TYPE_STRING = "comment";
 	Scanner in;
-	
-	Main(){
-		
-	}
-	
-	String getInput(Scanner in){
-		//reads input
-		return null;
-	}
-	
-	String checkInput(String inputString){
-		//checks input and returns the type of input as a String
-		return null;
-	}
-	
-	void processComment(String inputString) {
-		
+	// Table setTable;
+	PrintStream out;
+
+	Main() {
+		// setTable = new Table();
+		out = new PrintStream(System.out);
 	}
 
-	void processPrintStatement(String inputString) {
-		
+	String getInput(Scanner in) {
+		// reads input
+		return null;
 	}
 
-	void processAssignment(String inputString) {
-		
+	String checkInput(String inputString) {
+		// checks input and returns the type of input as a String
+		return null;
 	}
-	
-	void processInput(String inputString, String inputType){
-		if(inputType.equals(ASSIGNMENT_TYPE_STRING)){
+
+	void processPrintStatement(String printStatementString) {
+
+	}
+
+	void processAssignment(String assignmentString) {
+
+	}
+
+	void processInput(String inputString, String inputType) {
+		if (inputType.equals(ASSIGNMENT_TYPE_STRING)) {
 			processAssignment(inputString);
-		}else if(inputType.equals(PRINT_STATEMENT_TYPE_STRING)){
+		} else {
 			processPrintStatement(inputString);
-		}else{
-			processComment(inputString);
 		}
 	}
-	
 
-	void start(){
+	void start() {
 		in = new Scanner(System.in);
-		while(true){
+		while (true) {
 			String inputString = getInput(in);
 			String inputType = checkInput(inputString);
-			if(!inputType.equals(ERROR_TYPE_STRING)){
+			if (inputType.equals(ASSIGNMENT_TYPE_STRING) | inputType.equals(PRINT_STATEMENT_TYPE_STRING)) {
 				processInput(inputString, inputType);
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		new Main().start(); 
+		new Main().start();
 
 	}
 
