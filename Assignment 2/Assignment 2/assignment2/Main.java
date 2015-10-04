a581615cb751dcfdd0709b96baf5c020cb5f295f
ole380 <birdie_niels@hotmail.com>
2015-10-04 22:58:58 +0200

    main verder uitgewerkt

--- Assignment 2/Assignment 2/assignment2/Main.java
+++ Assignment 2/Assignment 2/assignment2/Main.java
@@ -2,14 +2,20 @@ package assignment2;
 
 import java.util.Scanner;
 import java.util.regex.Pattern;
+import java.beans.Expression;
 import java.io.PrintStream;
 
 public class Main {
 
 	static final char	COMMENT_TYPE_MARKER = '/',
-						PRINT_STATEMENT_TYPE_MARKER = '?';
+						PRINT_STATEMENT_TYPE_MARKER = '?',
+						IDENTIFIER_EXPRESSION_SEPERATOR = '=',
+						UNION_OPERATOR = '+',
+						COMPLEMENT_OPERATOR = '-',
+						SYMMETRIC_DIFFERENCE_OPERATOR = '|';
 
 	static final String LETTER_PATTERN = "[a-zA-Z]",
+						DIGIT_PATTERN = "[0-9]",
 						ERROR_TYPE_STRING = "error",
 						ASSIGNMENT_TYPE_STRING = "assignment",
 						PRINT_STATEMENT_TYPE_STRING = "print_statement",
@@ -31,44 +37,95 @@ public class Main {
 		return in.nextLine();
 	}
 
-	void processPrintStatement(String printStatementString) {
-
+	void readFactor(){
+		
+	}
+	
+	void readTerm(){
+		
+	}
+	
+	void readExpression(Scanner expressionScanner)throws APException{
+		readWhiteSpaces(expressionScanner);
+		readTerm();
+		while (expressionScanner.hasNext()){
+			if(nextCharIs(expressionScanner, UNION_OPERATOR)){
+				
+			}else if(nextCharIs(expressionScanner, COMPLEMENT_OPERATOR)){
+				
+			}else if(nextCharIs(expressionScanner, SYMMETRIC_DIFFERENCE_OPERATOR)){
+				
+			}else{
+				throw new APException("omdat er een teken is gevonden dat niet klopt");
+			}
+		}
+	}
+	
+	Identifier readIdentifier(Scanner identifierScanner)throws APException{
+		Identifier result;
+		if(nextCharIsLetter(identifierScanner)){
+			result = new Identifier(nextChar(identifierScanner));
+			while(!nextCharIs(identifierScanner, IDENTIFIER_EXPRESSION_SEPERATOR)){
+				readWhiteSpaces(identifierScanner);
+				if(nextCharIsAlphanumeric(identifierScanner)){
+					result.addCharacter(nextChar(identifierScanner));
+				}else{
+					throw new APException("Identifiers should only consist of alphanumeric characters and should be separated by \"=\".");
+				}
+			}
+		}else{
+			throw new APException("Identifiers should start with a letter.");
+			//This exception should not be thrown as this is checked earlier.
+		}
+		return result;
+	}
+	
+	void processPrintStatement(Scanner printStatementScanner) {
+		printStatementScanner.next();
 	}
 
-	void processAssignment(String assignmentString) {
-
+	void processAssignment(Scanner assignmentScanner)throws APException {
+		Identifier test = readIdentifier(assignmentScanner);
+		out.printf("%d", test.length());
 	}
 
+	char nextChar(Scanner in){
+		return in.next().charAt(0);
+	}
+	
+	boolean nextCharIsAlphanumeric(Scanner in){
+		return nextCharIsDigit(in)||nextCharIsLetter(in);
+	}
+	
 	boolean nextCharIs(Scanner in, char c){
 		return in.hasNext(Pattern.quote(c+""));
+	}
+
+	boolean nextCharIsDigit(Scanner in){
+		return in.hasNext(DIGIT_PATTERN);
 	}
 	
 	boolean nextCharIsLetter(Scanner in){
 		return in.hasNext(LETTER_PATTERN);
 	}
-	
+
 	void readWhiteSpaces(Scanner in){
 		while(in.hasNext(" ")){
 			in.next();
 		}
 	}
-	
-	String getInputType(Scanner inputScanner)throws APException{
+
+	void processInput(Scanner inputScanner)throws APException {
 		readWhiteSpaces(inputScanner);
 		if(nextCharIsLetter(inputScanner)){
-			return ASSIGNMENT_TYPE_STRING;
+			processAssignment(inputScanner);
 		}else if(nextCharIs(inputScanner, PRINT_STATEMENT_TYPE_MARKER)){
-			return PRINT_STATEMENT_TYPE_STRING;
+			processPrintStatement(inputScanner);
 		}else if(nextCharIs(inputScanner, COMMENT_TYPE_MARKER)){
-			return COMMENT_TYPE_STRING;
+			//does nothing when a comment is detected
 		}else{
 			throw new APException("Input error, expecting a letter, question mark or back-slash.");
 		}
-	}
-
-	void processInput(Scanner inputScanner)throws APException {
-		String inputType = getInputType(inputScanner);
-			out.printf("%s", inputType);
 	}
 
 	void runProgram(Scanner in)throws APException{
