d28ac47481ca8a26f9274d21009b1d6bf29e7034
ole380 <birdie_niels@hotmail.com>
2015-10-04 21:45:46 +0200

    expanded main

--- Assignment 2/Assignment 2/assignment2/Main.java
+++ Assignment 2/Assignment 2/assignment2/Main.java
@@ -1,14 +1,20 @@
 package assignment2;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 import java.io.PrintStream;
 
 public class Main {
 
-	static final String ERROR_TYPE_STRING = "error";
-	static final String ASSIGNMENT_TYPE_STRING = "assignment";
-	static final String PRINT_STATEMENT_TYPE_STRING = "print_statement";
-	static final String COMMENT_TYPE_STRING = "comment";
+	static final char	COMMENT_TYPE_MARKER = '/',
+						PRINT_STATEMENT_TYPE_MARKER = '?';
+
+	static final String LETTER_PATTERN = "[a-zA-Z]",
+						ERROR_TYPE_STRING = "error",
+						ASSIGNMENT_TYPE_STRING = "assignment",
+						PRINT_STATEMENT_TYPE_STRING = "print_statement",
+						COMMENT_TYPE_STRING = "comment";
+
 	Scanner in;
 	// Table setTable;
 	PrintStream out;
@@ -19,13 +25,10 @@ public class Main {
 	}
 
 	String getInput(Scanner in) {
-		// reads input
-		return null;
-	}
-
-	String checkInput(String inputString) {
-		// checks input and returns the type of input as a String
-		return null;
+		if(!in.hasNextLine()){
+			System.exit(0);
+		}
+		return in.nextLine();
 	}
 
 	void processPrintStatement(String printStatementString) {
@@ -36,23 +39,56 @@ public class Main {
 
 	}
 
-	void processInput(String inputString, String inputType) {
-		if (inputType.equals(ASSIGNMENT_TYPE_STRING)) {
-			processAssignment(inputString);
-		} else {
-			processPrintStatement(inputString);
+	boolean nextCharIs(Scanner in, char c){
+		return in.hasNext(Pattern.quote(c+""));
+	}
+	
+	boolean nextCharIsLetter(Scanner in){
+		return in.hasNext(LETTER_PATTERN);
+	}
+	
+	void readWhiteSpaces(Scanner in){
+		while(in.hasNext("")){
+			in.next();
 		}
+	}
+	
+	String getInputType(Scanner inputScanner)throws APException{
+		readWhiteSpaces(inputScanner);
+		if(nextCharIsLetter(inputScanner)){
+			return ASSIGNMENT_TYPE_STRING;
+		}else if(nextCharIs(inputScanner, PRINT_STATEMENT_TYPE_MARKER)){
+			return PRINT_STATEMENT_TYPE_STRING;
+		}else if(nextCharIs(inputScanner, COMMENT_TYPE_MARKER)){
+			return COMMENT_TYPE_STRING;
+		}else{
+			throw new APException("Input error, expecting a letter, question mark or back-slash");
+			
+		}
+	}
+
+	void processInput(Scanner inputScanner)throws APException {
+		String inputType = getInputType(inputScanner);
+			out.printf("%s", inputType);
+	}
+
+	void runProgram(Scanner in)throws APException{
+		String inputString = getInput(in);
+		Scanner inputScanner = new Scanner(inputString);
+		inputScanner.useDelimiter("");
+		processInput(inputScanner);
 	}
 
 	void start() {
 		in = new Scanner(System.in);
 		while (true) {
-			String inputString = getInput(in);
-			String inputType = checkInput(inputString);
-			if (inputType.equals(ASSIGNMENT_TYPE_STRING) | inputType.equals(PRINT_STATEMENT_TYPE_STRING)) {
-				processInput(inputString, inputType);
+			try {
+				runProgram(in);
+			} catch (APException e) {
+				out.printf("%s%", e.getMessage());
 			}
 		}
+
 	}
 
 	public static void main(String[] args) {
